fce896da212d65471f88c780b97fcfa2bdd06879
sooraj <soorajsuneesh@gmail.com>
2019-03-08 19:21:39 -0500

    date picker , picker view

--- Day5Sample/ViewController.swift
+++ Day5Sample/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+  
+    
+    let country = ["usa","canada","africa","india"
+                    ,"brazil","sri lanka","australia","china"]
+    
+    @IBOutlet weak var countrylabel: UILabel!
+    @IBOutlet weak var countrypicker: UIPickerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.countrypicker.delegate = self
+        self.countrypicker.dataSource = self
     }
 
 
+    @IBAction func BdayFigure(_ sender: UIDatePicker) {
+        print (sender.date)
+        print (sender)
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.country.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.country[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        countrylabel.text = self.country[row]
+    }
 }
-
